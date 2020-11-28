e352091cdac232293b8a1123245ce6f081809c46
aymenbahaoui <aymen.bahaoui@gmail.com>
2020-11-28 15:50:06 +0100

    first one in a while

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+// =======
+
+
+
+// == OR ==
+
+const http = require('http');
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
+
+server.listen(9000, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  });
+
+// ========
