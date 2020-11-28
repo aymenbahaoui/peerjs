536475bd5f13ae339a363fd8b9a2a7aa2337958a
aymenbahaoui <aymen.bahaoui@gmail.com>
2020-11-28 16:13:59 +0100

    dd

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use('/peerjs', peerServer);
 
-server.listen(9000, function () {
+server.listen(process.env.PORT, function () {
     console.log('CORS-enabled web server listening on port 80')
   });
 
